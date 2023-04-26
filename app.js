b3188bc6dd00f3497f13cfd867c554795edac486
kentakki416 <fujiken0814@outlook.jp>
2023-04-26 09:34:02 +0900

    クエリパラメータによるTodoのフィルタリング

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ let todos = [
 const app = express()
 
 // Todoの一覧取得
-app.get('/api/todos', (req, res) => res.json(todos))
+app.get('/api/todos', (req, res) => {
+  if (!req.query.completed) {
+    return res.json(todos)
+  }
+  // completedクエリパラメータを指定された場合はToDoをフィルタリング
+  const completed = req.query.completed === 'true'
+  res.json(todos.filter(todo => todo.completed === completed))
+})
 
 app.listen(3000)
