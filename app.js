42d303e5ef67d3abb6618c6d7c8e4b15f63c8bbd
kentakki416 <fujiken0814@outlook.jp>
2023-04-26 09:22:01 +0900

    /api/todosのハンドラーを作成

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+const express = require('express')
+
+let todos = [
+  { id: 1, title: 'ネーム', completed: false},
+  { id: 2, title: '下書き', completed: true}
+]
+const app = express()
+
+// Todoの一覧取得
+app.get('/api/todos', (req, res) => res.json(todos))
+
+app.listen(3000)
