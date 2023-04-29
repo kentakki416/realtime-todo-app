1e4c2287d126c2e51ea116e81eacf21dc587f623
kentakki416 <fujiken0814@outlook.jp>
2023-04-29 16:27:25 +0900

    putとdeleteの実装

--- app.js
+++ app.js
@@ -2,54 +2,87 @@
 const express = require('express')
 
 let todos = [
-    { id: 1, title: 'ネーム', completed: false},
-    { id: 2, title: '下書き', completed: true}
+  { id: 1, title: 'ネーム', completed: false },
+  { id: 2, title: '下書き', completed: true }
 ]
 const app = express()
+
 app.use(express.json())
 
-// Todoの一覧取得
+// ToDo一覧の取得
 app.get('/api/todos', (req, res) => {
-    if (!req.query.completed) {
-        return res.json(todos)
-    }
-    // completedクエリパラメータを指定された場合はToDoをフィルタリング
-    const completed = req.query.completed === 'true'
-    res.json(todos.filter(todo => todo.completed === completed))
+  if (!req.query.completed) {
+    return res.json(todos)
+  }
+  const completed = req.query.completed === 'true'
+  res.json(todos.filter(todo => todo.completed === completed))
 })
 
+// IDの値を管理するための変数
 let id = 2
 
+// ToDoの新規登録
 app.post('/api/todos', (req, res, next) => {
-    const {title} = req.body
-    if (typeof title !== 'string' || !title) {
-        const err = new Error('title is required')
-        err.statusCode = 400
-        return next(err)
-    }
-    // Todoの新規作成
-    const todo = { id: id +=1, title, completed: false }
-    todos.push(todo)
-    res.status(201).json(todo)
+  const { title } = req.body
+  if (typeof title !== 'string' || !title) {
+    // titleがリクエストに含まれない場合はステータスコード400（Bad Request）
+    const err = new Error('title is required')
+    err.statusCode = 400
+    return next(err)
+  }
+  const todo = { id: id += 1, title, completed: false }
+  todos.push(todo)
+  res.status(201).json(todo)
 })
 
+// 指定されたIDのToDoを取得するためのミドルウェア
+app.use('/api/todos/:id(\\d+)', (req, res, next) => {
+  const targetId = Number(req.params.id)
+  const todo = todos.find(todo => todo.id === targetId)
+  if (!todo) {
+    const err = new Error('ToDo not found')
+    err.statusCode = 404
+    return next(err)
+  }
+  // reqオブジェクトにtodoプロパティを追加
+  req.todo = todo
+  next()
+})
+
+// ToDoのCompletedの設定、解除
+app.route('/api/todos/:id(\\d+)/completed')
+  .put((req, res) => {
+    req.todo.completed = true
+    res.json(req.todo)
+  })
+  .delete((req, res) => {
+    req.todo.completed = false
+    res.json(req.todo)
+  })
+
+// ToDoの削除
+app.delete('/api/todos/:id(\\d+)', (req, res) => {
+  todos = todos.filter(todo => todo !== req.todo)
+  res.status(204).end()
+})
+
+// エラーハンドリングミドルウェア
 app.use((err, req, res, next) => {
-    console.log(err)
-    res.status(err.statusCode || 500).json({ error: err.message })
+  console.error(err)
+  res.status(err.statusCode || 500).json({ error: err.message })
 })
 
 app.listen(3000)
 
-// Next.jsによるルーティングのためこれ以降をついき
 const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
 
 nextApp.prepare().then(
-    // pageディレクトリ内の各Reactコンポーネントに対するサーバーサイドレンダリング
-    () => app.get('*', nextApp.getRequestHandler()),
-    err => {
-        console.error(err)
-        process.exit(1)
-    }
+  // pagesディレクトリ内の各Reactコンポーネントに対するサーバサイドルーティング
+  () => app.get('*', nextApp.getRequestHandler()),
+  err => {
+    console.error(err)
+    process.exit(1)
+  }
 )
